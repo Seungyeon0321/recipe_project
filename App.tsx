df56610c0b827bb3faf2e8680f147a29edc2d2fa
keitlyn-lau <78679013+keitlyn-lau@users.noreply.github.com>
2024-06-20 00:04:36 -0700

    Update App.tsx
    - Stylesheet is not necessarily needed here on this stage, thus removed for now
    - React Navigation shows the headers(title bars) by default. As requested, configuration is used to hide this default header for all the screens. Basically, if we want a full screen view, we use this configuration. ```options={{ headerShown: false }}```

--- App.tsx
+++ App.tsx
@@ -5,7 +5,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './main/navigation';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Greeting from "./screens/landing/greeting.screen";
-import { StyleSheet } from 'react-native';
 import Signup from "./screens/signup/signup.screen";
 import Login from "./screens/login/login.screen";
 
@@ -19,29 +18,14 @@ export default function App() {
   });
 
   return fontsLoaded ? (
-    <NavigationContainer style={styles.container}>
-      <Stack.Navigator initialRouteName="Greeting" style={styles.paragraph}>
-        <Stack.Screen name="Greeting" component={Greeting} options={{headerShown: false}} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="AppNavigator" component={AppNavigator} />
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Greeting">
+          <Stack.Screen name="Greeting" component={Greeting} options={{headerShown: false}} />
+          <Stack.Screen name="Signup" component={Signup} options={{headerShown: false}} />
+          <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+          <Stack.Screen name="AppNavigator" component={AppNavigator} options={{headerShown: false}} />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
   ) : null;
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  }
-});
+};
