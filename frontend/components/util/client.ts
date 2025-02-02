503fe42463cb14e66cd3807545a15661ebc4d934
Seungyeon Ji <jiseungyeon0321@gmail.com>
2025-02-02 05:56:53 -0800

    feat: Added createAsyncThunk

--- frontend/components/util/client.ts
+++ frontend/components/util/client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  // baseURL: "http://192.168.1.96:5000",
-  baseURL: "http://192.168.134.181:5000",
+  baseURL: "http://192.168.1.96:5000",
+  // baseURL: "http://192.168.134.181:5000",
   headers: {
     "Content-Type": "application/json",
   },
