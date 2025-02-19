a1739d0803c0fafa765dcddc4f70bfd31789da71
Seungyeon Ji <jiseungyeon0321@gmail.com>
2025-02-18 19:19:17 -0800

    feat: Added add_ingrediences componenet

--- frontend/screens/posting/posting.screen.tsx
+++ frontend/screens/posting/posting.screen.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import { Text, View, TextInput, Image } from "react-native";
+import React, { useState } from "react";
+import { Text, View, TextInput, Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { MainStyles } from "./posting.styles";
 import CustomButton from "../../components/UI/Button";
+import Add_ingrediences from "../../components/Posting/Add_ingrediences";
 
-export function PostingScreen() {
+export default function PostingScreen() {
   const {
     rootContainer,
     title,
@@ -41,26 +42,7 @@ export function PostingScreen() {
 
         <View>
           <Text style={title}>What's the ingredients?</Text>
-          <View style={ingredient_box}>
-            <Text style={number_text}>1</Text>
-            <View style={ing_input_container}>
-              <TextInput
-                style={[input_box, { flex: 4, marginRight: 10 }]}
-                placeholder="type here.."
-              />
-              <TextInput
-                style={[input_box, { flex: 3, marginRight: 10 }]}
-                placeholder="amount"
-              />
-              <TextInput style={[input_box, { flex: 2 }]} placeholder="unit" />
-            </View>
-          </View>
-          <View style={add_ing_container}>
-            <Image
-              style={add_image}
-              source={require("../../assets/images/posting_active.png")}
-            />
-          </View>
+          <Add_ingrediences />
         </View>
 
         <View>
